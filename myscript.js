b55f7bd87e38bdbe1a6a39de0707851c93bb6106
shivam <shivam.verma_cs19@gla.ac.in>
2022-04-25 14:59:28 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,24 @@
+//let var const 
+//let name = shivam; // 
+var like="pizza";
+const pi=3.14; // cannot define again
+let array =[1,2,3,4,5,'a','b'];
+let u= undefined;
+let n=null;
+let _NAN=NaN;
+let object={
+Keyname: "value"
+};
+
+// string
+//number
+//array[5] 
+//object.keyname or object["keyname"]
+
+function myfunc(){
+    //all code 
+    console.log("ABCD");
+}
+
+myfunc() //calling func
+myfunc()
